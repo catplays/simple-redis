2e6ba7a6c415063850e071124dd7b112456b8b15
catwang <865469028@qq.com>
2024-06-16 10:52:09 +0800

    feat: map and set decode

--- src/resp/decode.rs
+++ src/resp/decode.rs
@@ -78,14 +78,14 @@ impl RespDecode for RespFrame {
                 let frame = f64::decode(buf)?;
                 Ok(frame.into())
             }
-            // Some(b'%') => {
-            //     let frame = RespMap::decode(buf)?;
-            //     Ok(frame.into())
-            // }
-            // Some(b'~') => {
-            //     let frame = RespSet::decode(buf)?;
-            //     Ok(frame.into())
-            // }
+            Some(b'%') => {
+                let frame = RespMap::decode(buf)?;
+                Ok(frame.into())
+            }
+            Some(b'~') => {
+                let frame = RespSet::decode(buf)?;
+                Ok(frame.into())
+            }
             _ => Err(RespError::InvalidFrameType(format!(
                 "expect_length: unknown frame type: {:?}",
                 buf
@@ -97,8 +97,8 @@ impl RespDecode for RespFrame {
         let mut iter = buf.iter().peekable();
         match iter.peek() {
             Some(b'*') => RespArray::expect_length(buf),
-            // Some(b'~') => RespSet::expect_length(buf),
-            // Some(b'%') => RespMap::expect_length(buf),
+            Some(b'~') => RespSet::expect_length(buf),
+            Some(b'%') => RespMap::expect_length(buf),
             Some(b'$') => BulkString::expect_length(buf),
             Some(b':') => i64::expect_length(buf),
             Some(b'+') => SimpleString::expect_length(buf),
@@ -235,6 +235,21 @@ impl RespDecode for BulkString {
     }
 }
 
+impl RespDecode for f64 {
+    const PREFIX: &'static str = ",";
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
+        let end = extract_simple_frame_data(buf, Self::PREFIX)?;
+        let data = buf.split_to(end + CRLF_LEN);
+        let s = String::from_utf8_lossy(&data[Self::PREFIX.len()..end]);
+        Ok(s.parse()?)
+    }
+
+    fn expect_length(buf: &[u8]) -> Result<usize, RespError> {
+        let end = extract_simple_frame_data(buf, Self::PREFIX)?;
+        Ok(end + CRLF_LEN)
+    }
+}
+
 // "*<number-of-elements>\r\n<element-1>...<element-n>"
 //        - "*2\r\n$3\r\nget\r\n$5\r\nhello\r\n"
 impl RespDecode for RespArray {
@@ -261,20 +276,61 @@ impl RespDecode for RespArray {
     }
 }
 
-impl RespDecode for f64 {
-    const PREFIX: &'static str = ",";
+// "%<number-of-entries>\r\n<key-1><value-1>...<key-n><value-n>"
+// %2\r\n+hello\r\n$5\r\nworld\r\n+foo\r\n$3\r\nbar\r\n
+impl RespDecode for RespMap {
+    const PREFIX: &'static str = "%";
+
     fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
-        let end = extract_simple_frame_data(buf, Self::PREFIX)?;
-        let data = buf.split_to(end + CRLF_LEN);
-        let s = String::from_utf8_lossy(&data[Self::PREFIX.len()..end]);
-        Ok(s.parse()?)
+        let (end, len) = parse_length(buf, Self::PREFIX)?;
+        let total_len = calc_total_length(buf, end, len, Self::PREFIX)?;
+
+        if buf.len() < total_len {
+            return Err(RespError::NotComplete);
+        }
+        buf.advance(end + CRLF_LEN);
+        let mut map = RespMap::new();
+        for _ in 0..len {
+            let key = SimpleString::decode(buf)?;
+            let value = RespFrame::decode(buf)?;
+            map.insert(key.0, value);
+        }
+        Ok(map)
     }
 
     fn expect_length(buf: &[u8]) -> Result<usize, RespError> {
-        let end = extract_simple_frame_data(buf, Self::PREFIX)?;
-        Ok(end + CRLF_LEN)
+        let (end, len) = parse_length(buf, Self::PREFIX)?;
+        calc_total_length(buf, end, len, Self::PREFIX)
     }
 }
+
+impl RespDecode for RespSet {
+    const PREFIX: &'static str = "~";
+    fn decode(buf: &mut BytesMut) -> Result<Self, RespError> {
+        let (end, len) = parse_length(buf, Self::PREFIX)?;
+
+        let total_len = calc_total_length(buf, end, len, Self::PREFIX)?;
+
+        if buf.len() < total_len {
+            return Err(RespError::NotComplete);
+        }
+
+        buf.advance(end + CRLF_LEN);
+
+        let mut frames = Vec::new();
+        for _ in 0..len {
+            frames.push(RespFrame::decode(buf)?);
+        }
+
+        Ok(RespSet::new(frames))
+    }
+
+    fn expect_length(buf: &[u8]) -> Result<usize, RespError> {
+        let (end, len) = parse_length(buf, Self::PREFIX)?;
+        calc_total_length(buf, end, len, Self::PREFIX)
+    }
+}
+
 fn parse_length(buf: &[u8], prefix: &str) -> Result<(usize, usize), RespError> {
     let end = extract_simple_frame_data(buf, prefix)?;
     let s = String::from_utf8_lossy(&buf[prefix.len()..end]);
@@ -521,67 +577,67 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn test_double_decode() -> Result<()> {
-    //     let mut buf = BytesMut::new();
-    //     buf.extend_from_slice(b",123.45\r\n");
+    #[test]
+    fn test_double_decode() -> Result<()> {
+        let mut buf = BytesMut::new();
+        buf.extend_from_slice(b",123.45\r\n");
 
-    //     let frame = f64::decode(&mut buf)?;
-    //     assert_eq!(frame, 123.45);
+        let frame = f64::decode(&mut buf)?;
+        assert_eq!(frame, 123.45);
 
-    //     buf.extend_from_slice(b",+1.23456e-9\r\n");
-    //     let frame = f64::decode(&mut buf)?;
-    //     assert_eq!(frame, 1.23456e-9);
+        buf.extend_from_slice(b",+1.23456e-9\r\n");
+        let frame = f64::decode(&mut buf)?;
+        assert_eq!(frame, 1.23456e-9);
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 
-    // #[test]
-    // fn test_map_decode() -> Result<()> {
-    //     let mut buf = BytesMut::new();
-    //     buf.extend_from_slice(b"%2\r\n+hello\r\n$5\r\nworld\r\n+foo\r\n$3\r\nbar\r\n");
+    #[test]
+    fn test_map_decode() -> Result<()> {
+        let mut buf = BytesMut::new();
+        buf.extend_from_slice(b"%2\r\n+hello\r\n$5\r\nworld\r\n+foo\r\n$3\r\nbar\r\n");
 
-    //     let frame = RespMap::decode(&mut buf)?;
-    //     let mut map = RespMap::new();
-    //     map.insert(
-    //         "hello".to_string(),
-    //         BulkString::new(b"world".to_vec()).into(),
-    //     );
-    //     map.insert("foo".to_string(), BulkString::new(b"bar".to_vec()).into());
-    //     assert_eq!(frame, map);
+        let frame = RespMap::decode(&mut buf)?;
+        let mut map = RespMap::new();
+        map.insert(
+            "hello".to_string(),
+            BulkString::new(b"world".to_vec()).into(),
+        );
+        map.insert("foo".to_string(), BulkString::new(b"bar".to_vec()).into());
+        assert_eq!(frame, map);
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 
-    // #[test]
-    // fn test_set_decode() -> Result<()> {
-    //     let mut buf = BytesMut::new();
-    //     buf.extend_from_slice(b"~2\r\n$3\r\nset\r\n$5\r\nhello\r\n");
+    #[test]
+    fn test_set_decode() -> Result<()> {
+        let mut buf = BytesMut::new();
+        buf.extend_from_slice(b"~2\r\n$3\r\nset\r\n$5\r\nhello\r\n");
 
-    //     let frame = RespSet::decode(&mut buf)?;
-    //     assert_eq!(
-    //         frame,
-    //         RespSet::new(vec![
-    //             BulkString::new(b"set".to_vec()).into(),
-    //             BulkString::new(b"hello".to_vec()).into()
-    //         ])
-    //     );
+        let frame = RespSet::decode(&mut buf)?;
+        assert_eq!(
+            frame,
+            RespSet::new(vec![
+                BulkString::new(b"set".to_vec()).into(),
+                BulkString::new(b"hello".to_vec()).into()
+            ])
+        );
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 
-    // #[test]
-    // fn test_calc_array_length() -> Result<()> {
-    //     let buf = b"*2\r\n$3\r\nset\r\n$5\r\nhello\r\n";
-    //     let (end, len) = parse_length(buf, "*")?;
-    //     let total_len = calc_total_length(buf, end, len, "*")?;
-    //     assert_eq!(total_len, buf.len());
+    #[test]
+    fn test_calc_array_length() -> Result<()> {
+        let buf = b"*2\r\n$3\r\nset\r\n$5\r\nhello\r\n";
+        let (end, len) = parse_length(buf, "*")?;
+        let total_len = calc_total_length(buf, end, len, "*")?;
+        assert_eq!(total_len, buf.len());
 
-    //     let buf = b"*2\r\n$3\r\nset\r\n";
-    //     let (end, len) = parse_length(buf, "*")?;
-    //     let ret = calc_total_length(buf, end, len, "*");
-    //     assert_eq!(ret.unwrap_err(), RespError::NotComplete);
+        let buf = b"*2\r\n$3\r\nset\r\n";
+        let (end, len) = parse_length(buf, "*")?;
+        let ret = calc_total_length(buf, end, len, "*");
+        assert_eq!(ret.unwrap_err(), RespError::NotComplete);
 
-    //     Ok(())
-    // }
+        Ok(())
+    }
 }
